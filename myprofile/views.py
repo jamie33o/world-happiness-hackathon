40591549b22af6485ce02f231d0a54cd7d5d7c7a
sopiko abulidze <abulidze2019@gmail.com>
2024-03-23 15:31:10 +0000

    create profile html and add some content

--- myprofile/views.py
+++ myprofile/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def my_profile(request):
-    return render(request,'myprofile/profile.html')
+def profile_page(request):
+    user = request.user
+    context = {
+        "user": user
+    }
+    return render(request,'myprofile/profile.html', context)
